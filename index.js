d026652ddb6fe23184055383c1fde0b5f09b446b
Daniel Moi <dmoixy@gmail.com>
2018-10-22 05:54:31 +1100

    Add watchexec

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('MAGIC\n');
+  res.send('MAGIC!!\n');
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}`));
