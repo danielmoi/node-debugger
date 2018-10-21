8d58f2ae9057e0176a4037cb26849778578978a3
Daniel Moi <dmoixy@gmail.com>
2018-10-22 07:43:23 +1100

    Steps for basic node app

---
+++ README.md
@@ -0,0 +1,9 @@
+# Debugging Node Apps
+
+## Basic Node App
+- Start Node application (`yarn inspect`)
+- Open `http://localhost:1337`
+- Open Chrome Dev Tools
+- Click on the green Node.js icon (Open dedicated DevTools for Node.js)
+- Set a breakpoint on line 8
+- Use the step actions to see how `secret` is created
--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-const port = 3000;
+const PORT = 1337;
 
 app.get('/', (req, res) => {
   const secret = Math.floor(Math.random() * 10);
-
+  console.log('secret:', secret);
   res.send(`Secret: ${secret}\n`);
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
