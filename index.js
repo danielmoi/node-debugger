8d65b591a80dc902ab1c0f30d9a3162040939f4f
Daniel Moi <dmoixy@gmail.com>
2018-10-22 06:07:53 +1100

    Add secret

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('MAGIC!!\n');
+  const secret = Math.floor(Math.random() * 10);
+
+  res.send(`Secret: ${secret}\n`);
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
